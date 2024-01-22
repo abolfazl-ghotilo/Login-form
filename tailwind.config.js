37005173bf13298edb9f5a0077deec61608b8966
abolfazl <abolfazlghotilo@gmail.com>
2024-01-22 13:53:20 +0330

    feat:add header and title of form

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    colors:{
+      transparent:"transparent",
+      primary: "#280274",
+      secondary: "#424769",
+      tertiary: "#910A67",
+      black: "#000000",
+      white: "#f3f3f3",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
